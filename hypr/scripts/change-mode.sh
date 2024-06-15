9bbf95b83459cd056f64ff5324576d045f2b06c8
tiongg <tantiongguan123@gmail.com>
2024-06-15 23:56:11 +0800

    feat: update light mode

--- hypr/scripts/change-mode.sh
+++ hypr/scripts/change-mode.sh
@@ -1,13 +1,27 @@
 #!/usr/bin/env bash
 
-if [[ "$THEME" == "light" ]]; then
-  echo "Setting up light mode..."
-  wal -i $HOME/.config/wallpaper/light/login.png --saturate 0.7
-  
-else
-  echo "Setting up dark mode..."
-  wal -i $HOME/.config/wallpaper/dark/main.png --saturate 0.7
-fi
+# Updates stuff after changing the theme
+echo "Settings up " + $THEME + " mode..."
+
+# Update colors
+wal --theme ~/.config/wal/colorschemes/$THEME/custom-$THEME.json
+
+# Updates sddm
+SSDM_THEME="sugar-candy"
+sudo cp ~/.config/sddm/theme-$THEME.conf /usr/share/sddm/themes/$SSDM_THEME/theme.conf
+
+## Used to have different imgs for different schemes
+# if [[ "$THEME" == "light" ]]; then
+#   echo "Setting up light mode..."
+
+#   # Colors
+#   # wal -i $HOME/.config/wallpaper/light/login.png --saturate 0.7
+#   wal --theme ~/.config/wal/colorschemes/light/custom-light.json
+# else
+#   echo "Setting up dark mode..."
+#   # wal -i $HOME/.config/wallpaper/dark/main.png --saturate 0.7
+#   wal --theme ~/.config/wal/colorschemes/dark/custom-dark.json
+# fi
 
 echo "Done, restart hyprland"
 echo "TODO: Make it so I don't"
