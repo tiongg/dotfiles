942b6fa103737c2376d24662338fff737ae4da13
tiongg <tantiongguan123@gmail.com>
2024-06-17 03:05:51 +0800

    chore: swap light mode lock and main wallpaper

--- README.md
+++ README.md
@@ -26,4 +26,5 @@ https://github.com/zDyanTB/HyprNova/tree/master
 ## Random notes:
 Remember to symlink zsh config!!
 Set sddm theme here: /usr/lib/sddm/sddm.conf.d/default.conf
-Send notification: notify-send -u low "test"
+Send notification: notify-send 'Hello world!' 'This is an example notification.' --icon=dialog-information -a telegam-desktop -t 9999999
+Start sway with css: GTK_DEBUG=interactive swaync
--- hypr/scripts/change-mode.sh
+++ hypr/scripts/change-mode.sh
@@ -4,6 +4,7 @@
 echo "Settings up " + $THEME + " mode..."
 
 # Update colors
+# wal -i $HOME/.config/wallpaper/$THEME/main.png --saturate 0.7
 wal --theme ~/.config/wal/colorschemes/$THEME/custom-$THEME.json
 
 # Updates sddm
@@ -11,19 +12,6 @@ wal --theme ~/.config/wal/colorschemes/$THEME/custom-$THEME.json
 SSDM_THEME="sugar-candy"
 sudo cp ~/.config/sddm/theme-$THEME.conf /usr/share/sddm/themes/$SSDM_THEME/theme.conf
 sudo cp ~/.config/wallpaper/$THEME/login.png /usr/share/sddm/themes/$SSDM_THEME/Backgrounds/1.png
-
-## Used to have different imgs for different schemes
-# if [[ "$THEME" == "light" ]]; then
-#   echo "Setting up light mode..."
-
-#   # Colors
-#   # wal -i $HOME/.config/wallpaper/light/login.png --saturate 0.7
-#   wal --theme ~/.config/wal/colorschemes/light/custom-light.json
-# else
-#   echo "Setting up dark mode..."
-#   # wal -i $HOME/.config/wallpaper/dark/main.png --saturate 0.7
-#   wal --theme ~/.config/wal/colorschemes/dark/custom-dark.json
-# fi
 
 echo "Done, restart hyprland"
 echo "TODO: Make it so I don't"
