58c8549291fc82d3d0d6bfa53fdf3685e1a42f65
tiongg <tantiongguan123@gmail.com>
2024-07-06 17:22:37 +0800

    chore(ags): log scss errors

--- ags/styles/style-helper.ts
+++ ags/styles/style-helper.ts
@@ -11,16 +11,20 @@ const $ = (name: string, value: string) => `$${name}: ${value};`;
 const variables = [$('white', '#fff'), $('black', '#000')];
 
 export async function resetCss() {
-  const fd = await bash(`fd ".scss" ${App.configDir}/styles`);
-  const walPath = await bash('echo $HOME/.cache/wal/colors.scss');
-  const files = fd.split(/\s+/);
-  const imports = [variablesPath, walPath, ...files].map(
-    (f) => `@import "${f}";`
-  );
+  try {
+    const fd = await bash(`fd ".scss" ${App.configDir}/styles`);
+    const walPath = await bash('echo $HOME/.cache/wal/colors.scss');
+    const files = fd.split(/\s+/);
+    const imports = [variablesPath, walPath, ...files].map(
+      (f) => `@import "${f}";`
+    );
 
-  await Utils.writeFile(variables.join('\n'), variablesPath);
-  await Utils.writeFile(imports.join('\n'), scssPath);
-  await bash(`sass ${scssPath} ${finalCssPath}`);
+    await Utils.writeFile(variables.join('\n'), variablesPath);
+    await Utils.writeFile(imports.join('\n'), scssPath);
+    await bash(`sass ${scssPath} ${finalCssPath}`);
 
-  App.applyCss(finalCssPath, true);
+    App.applyCss(finalCssPath, true);
+  } catch (e) {
+    console.error(e);
+  }
 }
