f5cbc9a95c3cd454febeaf43a176617fb8659921
tiongg <tantiongguan123@gmail.com>
2024-07-12 13:31:17 +0800

    feat(ags): add new shell util

---
+++ ags/src/utils/utils.ts
@@ -0,0 +1,23 @@
+/**
+ * Execute Bash command(s)
+ * @param commands - Command(s) to execute
+ * @param args - Arguments to pass to the command(s). Maps to the commands at the same index
+ * @returns Bash command output
+ */
+export async function bash(commands: string | string[], ...args: unknown[]) {
+  //Map commands to args
+  const cmd = Array.isArray(commands)
+    ? commands.map((c, i) => `${c} ${args[i] ?? ''}`).join(' ')
+    : commands;
+
+  return Utils.execAsync(['bash', '-c', cmd]);
+}
+
+/**
+ * Run a shell command
+ * @param cmd - Command to run
+ * @returns Shell command output
+ */
+export async function sh(cmd: string | string[]) {
+  return Utils.execAsync(cmd);
+}
--- ags/utils/utils.ts
+++
@@ -1,8 +0,0 @@
-export async function bash(commands: string | string[], ...args: unknown[]) {
-  //Map commands to args
-  const cmd = Array.isArray(commands)
-    ? commands.map((c, i) => `${c} ${args[i] ?? ''}`).join(' ')
-    : commands;
-
-  return Utils.execAsync(['bash', '-c', cmd]);
-}
