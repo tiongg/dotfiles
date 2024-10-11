5eaf97ab439bca3a7fce1e68e4668f9628fe7bda
tiongg <tantiongguan123@gmail.com>
2024-10-11 10:24:39 +0800

    feat(ags): add icon util function

--- ags/src/utils/utils.ts
+++ ags/src/utils/utils.ts
@@ -1,4 +1,5 @@
 import Gdk from 'gi://Gdk';
+import GLib from 'gi://GLib?version=2.0';
 import Gtk from 'gi://Gtk?version=3.0';
 import _ from 'lodash';
 
@@ -26,7 +27,26 @@ export async function sh(cmd: string | string[]) {
   return Utils.execAsync(cmd);
 }
 
+/**
+ * Creates an individual widget for each monitor
+ * @param widget - Widget that takes in monitor index
+ * @returns Array of widgets for each monitor
+ */
 export function forMonitors(widget: (monitor: number) => Gtk.Window) {
   const monitors = Gdk.Display.get_default()?.get_n_monitors() || 1;
   return _.range(monitors).flatMap(widget);
+}
+
+/**
+ * Checks if icon exists, if not return fallback
+ * @returns icon or fallback
+ */
+export function iconOrFallback(
+  icon: string | null,
+  fallback = 'image-missing-symbolic'
+) {
+  if (!icon) return fallback;
+  if (GLib.file_test(icon, GLib.FileTest.EXISTS)) return icon;
+  if (Utils.lookUpIcon(icon)) return icon;
+  return fallback;
 }
